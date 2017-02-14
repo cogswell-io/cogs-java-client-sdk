98b1f70370a20c22b1f5d6fe5e55697eabad53da
David Lewton <dlewton@aviatainc.com>
2017-02-13 17:19:59 -0700

    fixed minor error in javadocs

--- src/main/java/com/gambit/sdk/pubsub/utils/PubSubUtils.java
+++ src/main/java/com/gambit/sdk/pubsub/utils/PubSubUtils.java
@@ -8,7 +8,6 @@ public class PubSubUtils {
      * Simple method to spin up new thread that calls provided Runnable no sooner than the given delay in ms.
      * @param runnable The runnable that will be called after the given delay
      * @param delay The time in milliseconds to wait before calling the given runnable
-     * @throws InterruptedException
      */
     public static void setTimeout(Runnable runnable, long delay)
     {
